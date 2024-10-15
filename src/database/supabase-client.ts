89007825b793a3615e83aaef96a7e7f46ebdec51
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-15 15:22:52 +0100

    refactor: added .env variables

--- src/database/supabase-client.ts
+++ src/database/supabase-client.ts
@@ -1,6 +1,5 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 interface ProcessEnv {
@@ -8,7 +7,13 @@ interface ProcessEnv {
   SUPABASE_KEY: string;
 }
 
-const { SUPABASE_URL, SUPABASE_KEY } = process.env as unknown as ProcessEnv;
+const env = process.env as unknown as ProcessEnv;
+
+const SUPABASE_URL = env.SUPABASE_URL;
+const SUPABASE_KEY = env.SUPABASE_KEY;
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase: SupabaseClient = createClient(
   SUPABASE_URL,
