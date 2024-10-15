216bcc65d84727e0dae443e1547fe185688a9f56
JoshCodedit <160856275+JoshCodedit@users.noreply.github.com>
2024-10-15 15:27:42 +0100

    Update src/database/supabase-client.ts
    Co-authored-by: Maxime Downe <81163597+maxitect@users.noreply.github.com>

--- src/database/supabase-client.ts
+++ src/database/supabase-client.ts
@@ -7,10 +7,8 @@ interface ProcessEnv {
   SUPABASE_KEY: string;
 }
 
-const env = process.env as unknown as ProcessEnv;
-
-const SUPABASE_URL = env.SUPABASE_URL;
-const SUPABASE_KEY = env.SUPABASE_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error("Missing Supabase environment variables");
 }
