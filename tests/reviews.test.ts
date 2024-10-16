c43175cadeaded5ca10208a6dc39a0ee13ed4f36
maxitect <maxime.downe@gmail.com>
2024-10-16 12:52:19 +0100

    test: initial endpoint testing for TDD

---
+++ tests/reviews.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import { app } from '../src/index';
+
+describe('POST /reviews', () => {
+    it('should create a new review', async () => {
+      const newReview = {
+        score: 5,
+        description: 'Great vinyl!',
+        vinyl_id: 1,
+        transaction_id: 1
+      };
+      const response = await request(app).post('/reviews').send(newReview);
+      expect(response.status).toBe(201);
+      expect(response.body.score).toBe(newReview.score);
+    });
+});
