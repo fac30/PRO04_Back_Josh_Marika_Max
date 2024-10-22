ce550a0400cf8b93719a9e91ea5dd99d3b84db1e
maxitect <maxime.downe@gmail.com>
2024-10-22 17:57:13 +0100

    feat: add join to /transactions endpoint to join content from statuses and shipping_options tables

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,10 +4,24 @@ import { Transaction } from "../utils/schemaTypes"
 
 const router = Router();
 
-router.get('/transactions/:id', async (req: Request, res: Response) => {
+router.get('/transactions/:customer_id', async (req: Request, res: Response) => {
     try {
-      const { id } = req.params;
-      const transactions: Transaction[] = await dbGet<Transaction>('customers');
+      const { customer_id } = req.params;
+      const transactions: Transaction[] = await dbGet<Transaction>('customers', {
+        where: { customer_id },
+        join: [
+            {
+              relatedTable: 'statuses',
+              foreignKey: 'status_id',
+              columns: 'status'
+            },
+            {
+              relatedTable: 'shipping_options',
+              foreignKey: 'shipping_option_id',
+              columns: 'shipping_option'
+            }
+        ]
+      });
       res.status(200).json(transactions);
     } catch (error) {
       console.error(error);
