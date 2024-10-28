bc9ff3397d0ab9a17edc96eb31e7c005a3e7785b
maxitect <maxime.downe@gmail.com>
2024-10-28 18:01:09 +0000

    fix: register route now working with express session

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,31 +1,17 @@
-import { Request, Response, Router } from 'express';
-import { dbGet } from '../models/dbGet';
-import { dbPost } from '../models/dbPost';
+import { Router, Request, Response } from 'express';
+import { supabase } from "../database/supabaseClient";
 import { Customer } from "../utils/schemaTypes";
 
 const router = Router();
 
-router.post('/register', async (req: Request, res: Response) => {
-    try {
-      const customer = await dbPost('customers', req.body);
-      if (!customer || !customer.id) {
-        throw new Error('Failed to insert customer');
-      }
-      const updatedCustomer = await dbGet<Customer>('customers', {
-        where: { id: customer.id },
-        join: {
-          relatedTable: 'locations',
-          foreignKey: 'location_id',
-          columns: ['region', 'country']
-        }
-      });
-      console.log(updatedCustomer);
-      const session = await dbPost('sessions', { customer_id: customer.id });
-      res.status(201).json({ customer: updatedCustomer[0], session });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: `Error registering customer and creating session`, error });
+export default router.post('/register', async (req: Request<{}, {}, Partial<Customer>>, res: Response) => {
+    const { data, error } = await supabase.from('customers').insert([req.body]).select('*');
+    if (error) {
+        res.status(400).json({ message: 'Registration failed', error });
+    } else if (data && data.length > 0) {
+        const { password_hash, ...customerData } = data[0];
+        res.status(201).json({ message: 'Signup successful', customer: customerData });
+    } else {
+        res.status(500).json({ message: 'Unexpected error occurred' });
     }
 });
-
-export default router;
