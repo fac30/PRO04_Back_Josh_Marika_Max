4eeaf5780d560593f3b7926ac05f1688323568c0
maxitect <maxime.downe@gmail.com>
2024-10-28 18:02:00 +0000

    fix: login route WIP - using supabase auth perhaps we need raw passwords - tbc

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,48 +1,24 @@
-import { Request, Response, Router } from 'express';
-import { dbGet } from '../models/dbGet';
-import { dbPost } from '../models/dbPost';
-import { dbDelete } from '../models/dbDelete';
-import { Customer, Session } from "../utils/schemaTypes";
+import { Router, Request, Response } from 'express';
+import { supabase } from "../database/supabaseClient";
+import { Customer } from "../utils/schemaTypes";
 
 const router = Router();
 
-router.post('/login', async (req: Request, res: Response) => {
-    try {
-      const { username, password_hash } = req.body;
-      
-      const customers: Customer[] = await dbGet<Customer>('customers', {
-        where: { username },
-        join: {
-          relatedTable: 'locations',
-          foreignKey: 'location_id',
-          columns: ['region', 'country']
-        }
-      });
-  
-      if (customers.length === 0 || customers[0].password_hash != password_hash) {
-        res.status(401).json({ message: 'Invalid username or password' });
-        return;
-      }
-  
-      const customer = customers[0];
-      const sessions = await dbGet<Session>('sessions', { where: { customer_id: customer.id } });
-  
-      if (sessions.length > 0) {
-        const session = sessions[0];
-        await dbDelete('sessions', session.id);
-      }
-  
-      const newSession = await dbPost('sessions', {
-        customer_id: customer.id,
-        expires_at: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      });
-  
-      res.status(201).json({ message: 'Login successful', customer, session: newSession });
-    } catch (error) {
-      console.error(error);
-  
-      res.status(500).json({ message: 'Error during login', error });
-    }
-});
+declare module 'express-session' {
+  interface SessionData {
+      userId: string;
+  }
+}
 
-export default router;
+type LoginRequestBody = Pick<Customer, 'email' | 'password_hash'>;
+
+export default router.post('/login', async (req: Request<{}, {}, LoginRequestBody>, res: Response) => {
+    const { email, password_hash } = req.body;
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password: password_hash });
+    if (data?.session) {
+        req.session.userId = data.user.id;
+        res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(400).json({ error: error?.message || 'Login failed' });
+    }
+})
