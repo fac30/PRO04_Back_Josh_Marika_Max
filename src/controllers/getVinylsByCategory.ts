2be117f42693c49c3c0e3579bf181bdb15ddd256
maxitect <maxime.downe@gmail.com>
2024-10-28 17:00:10 +0000

    feat: add controllers for category vinyl endpoints

---
+++ src/controllers/getVinylsByCategory.ts
@@ -0,0 +1,50 @@
+import { supabase } from "../database/supabaseClient";
+
+export interface Category {
+    id: number;
+    [key: string]: string | number;
+}
+
+export async function getCategory(category: string): Promise<Category[]> {
+    try {
+        const response = await supabase
+            .from(`${category}s`)
+            .select(`id, ${category}`);
+        if (response.error || !response.data) {
+            console.error(`Error fetching ${category} data:`, response.error);
+            return [];
+        }
+        const data = response.data as unknown;
+        if (Array.isArray(data) && data.every(item => typeof item === 'object' && item !== null && 'id' in item && typeof item.id === 'number')) {
+            return data as Category[];
+        } else {
+            console.error(`Data fetched from ${category} does not match Category[] format.`);
+            return [];
+        }
+    } catch (error) {
+        console.error(`Unexpected error fetching ${category} data:`, error);
+        return [];
+    }
+}
+
+export async function getVinylsByCategory(categoryData: Category, category: string) {
+    try {
+        const vinylsData = await supabase
+            .from('vinyls')
+            .select(`
+                *,
+                genres (genre),
+                labels (label),
+                conditions (condition),
+                price_ranges (price_range),
+                time_periods (time_period),
+                collection_types (collection_type),
+                discs (*)
+            `)
+            .eq(`${category}_id`, categoryData.id);
+        return vinylsData;
+    } catch (error) {
+        console.error(`Unexpected error fetching ${category} data:`, error);
+        return [];
+    }
+}
