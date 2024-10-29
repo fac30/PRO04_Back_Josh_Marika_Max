1c5592b13e6d416f8ee6b053415310dcaf4f861f
maxitect <maxime.downe@gmail.com>
2024-10-29 11:38:54 +0000

    fix: move session secret to .env file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
+import dotenv from "dotenv";
+
 import genericEndpoints from './routes/genericEndpoints';
 import register from './routes/register';
 import login from './routes/login';
@@ -11,15 +13,17 @@ import transactions from './routes/transactions';
 import shippingOptions from './routes/shippingOptions';
 import vinylsByCategory from './routes/vinylsByCategory';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const sessionSecret = process.env.SESSION_SECRET;
 
 app.use(express.json());
 app.use(cors());
 
 app.use(
   session({
-      secret: 'sEcReT1997-hAhAhA!!',
+      secret: sessionSecret as string,
       resave: false,
       saveUninitialized: true,
       cookie: { secure: false, httpOnly: true },
