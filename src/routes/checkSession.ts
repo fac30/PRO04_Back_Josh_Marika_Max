e7414762a9d85f561d93c7aa9d26c093702a5fa5
maxitect <maxime.downe@gmail.com>
2024-10-31 11:49:39 +0000

    fix: check-session endpoint updated with isLoggedIn boolean and returning customer object

--- src/routes/checkSession.ts
+++ src/routes/checkSession.ts
@@ -1,11 +1,30 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response } from "express";
+import { supabase } from "../database/supabaseClient";
 
-const router = Router()
+const router = Router();
 
-export default router.get('/check-session', (req: Request, res: Response) => {
+export default router.get(
+  "/check-session",
+  async (req: Request, res: Response) => {
     if (req.session.userId) {
-        res.status(200).json({ loggedIn: true, userId: req.session.userId })
+      const { data: customerData, error } = await supabase
+        .from("customers")
+        .select("*, locations (region, country)")
+        .eq("id", req.session.userId)
+        .single();
+      if (error || !customerData) {
+        res
+          .status(400)
+          .json({ error: "User does not exist", isLoggedIn: false });
+        return;
+      } else {
+        const { password_hash, ...customer } = customerData;
+        res
+          .status(200)
+          .json({ message: "User is logged in", isLoggedIn: true, customer });
+      }
     } else {
-        res.status(401).json({ loggedIn: false, message: 'Not logged in' })
+      res.status(401).json({ isLoggedIn: false, message: "Not logged in" });
     }
-});
+  }
+);
