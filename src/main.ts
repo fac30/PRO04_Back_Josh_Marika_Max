3fc9251aca59ae121a826256b3355e727e64f0c7
maxitect <maxime.downe@gmail.com>
2024-10-31 12:17:42 +0000

    fix: added localhost 5173 and 5174 to CORS policy

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
+import express from "express";
+import cors from "cors";
+import session from "express-session";
 import dotenv from "dotenv";
 
-import swaggerDocs from './swagger';
+import swaggerDocs from "./swagger";
 
-import genericEndpoints from './routes/genericEndpoints';
-import register from './routes/register';
-import login from './routes/login';
-import logout from './routes/logout';
-import checkSession from './routes/checkSession';
-import vinyls from './routes/vinyls';
-import vinylsByCategory from './routes/vinylsByCategory';
-import transactions from './routes/transactions';
-import transactionsByStatus from './routes/transactionsByStatus';
-import shippingOptions from './routes/shippingOptions';
+import genericEndpoints from "./routes/genericEndpoints";
+import register from "./routes/register";
+import login from "./routes/login";
+import logout from "./routes/logout";
+import checkSession from "./routes/checkSession";
+import vinyls from "./routes/vinyls";
+import vinylsByCategory from "./routes/vinylsByCategory";
+import transactions from "./routes/transactions";
+import transactionsByStatus from "./routes/transactionsByStatus";
+import shippingOptions from "./routes/shippingOptions";
 
 dotenv.config();
 const app = express();
@@ -22,14 +22,33 @@ const port = process.env.PORT || 3000;
 const sessionSecret = process.env.SESSION_SECRET;
 
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+  "https://localhost:5173",
+  "https://localhost:5174",
+  "http://pro04cdkstack-sitebucket397a1860-yjssa5llco8j.s3-website.eu-west-2.amazonaws.com",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(
   session({
-      secret: sessionSecret as string,
-      resave: false,
-      saveUninitialized: true,
-      cookie: { secure: false, httpOnly: true },
+    secret: sessionSecret as string,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: true },
   })
 );
 
@@ -45,7 +64,6 @@ app.use(vinylsByCategory);
 app.use(transactions);
 app.use(transactionsByStatus);
 app.use(shippingOptions);
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
